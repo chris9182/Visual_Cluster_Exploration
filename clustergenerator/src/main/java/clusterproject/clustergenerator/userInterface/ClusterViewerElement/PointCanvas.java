73e0b839d79d0330553c06adb7c3bde318b7db19
Christian Permann <christian@permann.eu>
2018-10-31 16:01:36 +0100

    slight performance improvement but may be usefull later

--- clustergenerator/src/main/java/clusterproject/clustergenerator/userInterface/ClusterViewerElement/PointCanvas.java
+++ clustergenerator/src/main/java/clusterproject/clustergenerator/userInterface/ClusterViewerElement/PointCanvas.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.util.List;
+import java.util.stream.IntStream;
 
 import javax.swing.JPanel;
 
@@ -34,17 +36,25 @@ public class PointCanvas extends JPanel {
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
 		final int pointWidth = clusterViewer.getPointDiameter();
+		final int pointCount = pointContainer.getPoints().size();
+		final int[] yCoordinates = new int[pointCount];
+		final int[] xCoordinates = new int[pointCount];
 
-		for (final double[] position : pointContainer.getPoints()) {
-			final int[] pixel = clusterViewer.getPixel(position);// TODO: this could be fetched in parallel
-			if (pixel == null)
-				continue;
+		final List<double[]> points = pointContainer.getPoints();
+		final IntStream stream = IntStream.range(0, pointCount);
+
+		stream.parallel().forEach(value -> {
+			final int[] pixel = clusterViewer.getPixel(points.get(value));
+			xCoordinates[value] = pixel[0];
+			yCoordinates[value] = pixel[1];
+		});
+
+		for (int i = 0; i < pointCount; ++i) {
 			g2.setColor(Color.GRAY);
-			g2.fillOval(pixel[0] - pointWidth / 2, pixel[1] - pointWidth / 2, pointWidth, pointWidth);
+			g2.fillOval(xCoordinates[i] - pointWidth / 2, yCoordinates[i] - pointWidth / 2, pointWidth, pointWidth);
 			g2.setColor(Color.BLACK);
-			g2.drawOval(pixel[0] - pointWidth / 2, pixel[1] - pointWidth / 2, pointWidth, pointWidth);
+			g2.drawOval(xCoordinates[i] - pointWidth / 2, yCoordinates[i] - pointWidth / 2, pointWidth, pointWidth);
 		}
-
 	}
 
 }
