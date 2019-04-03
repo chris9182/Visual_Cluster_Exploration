431e5c34d899dc3ba8e589d0aae74c1a4b36893f
Christian Permann <christian@permann.eu>
2019-04-03 08:52:31 +0200

    possible graph partitioning lib

--- clustergenerator/src/main/java/clusterproject/App.java
+++ clustergenerator/src/main/java/clusterproject/App.java
@@ -6,19 +6,11 @@ import javax.swing.JFrame;
 
 import clusterproject.program.MainWindow;
 
-/**
- * Hello world!
- *
- */
 public class App {
 	public static void main(String[] args) {
-		/*
-		 * final Gpmetis metis = new Gpmetis(); final Grph g = new InMemoryGrph();
-		 * g.addUndirectedSimpleEdge(0, 0); g.addUndirectedSimpleEdge(1, 0);
-		 * g.addUndirectedSimpleEdge(1, 2); g.addUndirectedSimpleEdge(0, 2);
-		 * metis.compute(g, 2, new Random());
-		 */
-		//TODO: could be looked at for graph partitioning
+		// TODO: could be looked at for graph partitioning
+		// org.apache.giraph.partition.GraphPartitionerFactory<WritableComparable,
+		// Writable, Writable>
 
 		final MainWindow window = new MainWindow();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
