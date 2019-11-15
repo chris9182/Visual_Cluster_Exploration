7eb270efb7f31a75709ce38fe1fe4c4de2586bec
Chris <chris9182@gmail.com>
2019-11-15 17:26:55 +0100

    removed syserr

--- clustergenerator/src/main/java/clusterproject/program/ImporterWindow.java
+++ clustergenerator/src/main/java/clusterproject/program/ImporterWindow.java
@@ -202,7 +202,6 @@ public class ImporterWindow extends JFrame {
 				labelIndex = size - 1;
 
 			final boolean hasLabels = labelIndex > -1;
-			System.err.println(hasLabels ? size - 1 : size);
 			pointContainer.setDim(hasLabels ? size - 1 : size);
 			final CSVRecord firstRecord = records.get(0);
 			boolean hasHeaders = false;
